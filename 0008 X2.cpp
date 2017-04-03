c6c6d733ecb8f7b8ee0410c300df22c296e52fdf
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-04-03 23:23:25 +0700

    0008
    Use nothing just an easy one.

---
+++ 0008 X2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include <math.h>
+//#include <stdlib.h>
+//#include <iomanip>
+//#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << b*2-a;
+    return 0;
+}
