1b3470d6c33f0c1629c483946305b2bd8acdaf2e
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-04-03 23:40:07 +0700

    0010
    string,array,swap

---
+++ 0010 Trik.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+//#include <math.h>
+//#include <stdlib.h>
+//#include <iomanip>
+//#include <queue>
+
+using namespace std;
+
+int main()
+{
+    string in;
+    int i=0;
+    char a[3];
+    a[0] = 1;
+    a[1] = 0;
+    a[2] = 0;
+    cin >> in;
+    while(in[i]!='\0')
+    {
+        if(in[i]=='A')
+            swap(a[0],a[1]);
+        else if(in[i]=='B')
+            swap(a[1],a[2]);
+        else
+            swap(a[0],a[2]);
+        i++;
+    }
+    for(int i=0;i<3;i++)
+    {
+        if(a[i]==1)
+            cout << i+1;
+    }
+    return 0;
+}
